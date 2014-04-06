733edb7b3122c3f5a567643861dadd22a1041af2
Alex.OConnor@scss.tcd.ie <Alex.OConnor@scss.tcd.ie>
2014-04-06 18:04:58 +0100

    had to install pdfminer to get cmap-safe encoding working. pip --pre pdfminer installs it. moved pypdf functions to splitpapers

--- splitpapers.py
+++ splitpapers.py
@@ -2,7 +2,7 @@
 
 # -*- coding: utf-8 -*-
 
-""" extract pages from the document """
+""" split pages from the document """
 from __future__ import unicode_literals
 
 import os
@@ -10,8 +10,7 @@ import sys
 import codecs
 
 from PyPDF2 import PdfFileReader
-from ftfy import fix_text, guess_bytes
-
+from PyPDF2 import PdfFileWriter
 
 __author__ = "Alexander O'Connor <oconnoat@gmail.com>"
 __copyright__ = "Copyright 2012, Alexander O'Connor <oconnoat@gmail.com>"
@@ -21,13 +20,18 @@ __version__ = "0.1"
 __email__ = "Alexander O'Connor <oconnoat@gmail.com>"
 __status__ = "Prototype"
 
-def extract_papers(pdf_file, paper_pages):
-    """Take the PDF object and the page delimiters and split them"""
+def split_papers(pdf_file, paper_pages):
+    """Take the PDF filepath and the page delimiters and split them"""
+    pdf_reader = PdfFileReader(file(pdf_file,'rb'))
+
     for paper in paper_pages:
-        with open( os.path.join('extracted_text', paper+'.txt'), 'w') as text_file:
+        pdf_writer = PdfFileWriter()
             #explode the tuple range
-            for page in range(*paper_pages[paper]):
-                text_file.write(pdf_file.getPage(page).extractText().encode('utf-8'))
+        for page in range(*paper_pages[paper]):
+            pdf_writer.addPage(pdf_reader.getPage(page))
+
+    pdf_writer.write(file(os.path.join('extracted_text',paper),'wb'))
+
 
 if __name__ == '__main__':
     data_filename = os.path.join('data','TraumaPapers_2009.pdf')
@@ -39,8 +43,5 @@ if __name__ == '__main__':
 #                   'paper2' : (28, 65),
 #                   'paper3' : (65,104) }
 
-    with open(data_filename, 'rb') as data_file:
-        data_pdf = PdfFileReader(data_file)
-
-        extract_papers(data_pdf, paper_pages)
+    split_papers(data_filename, paper_pages)
 
